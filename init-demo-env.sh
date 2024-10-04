624f0da9fd27f95191acb15cfee19a17ce233152
Eric Allard <106756852+SOOS-EAllard@users.noreply.github.com>
2024-10-04 16:41:09 -0400

    tweaks

--- init-demo-env.sh
+++ init-demo-env.sh
@@ -2,6 +2,11 @@
 # Ensure fresh installation of SOOS package
 [ -d "./soos" ] && rm -rf "./soos"
 npm install --prefix ./soos @soos-io/soos-sca
+npm install --prefix ./soos @soos-io/soos-sbom
+npm install --prefix ./soos @soos-io/soos-sast
+
+docker pull soosio/dast
+docker pull soosio/csa
 
 # Open a shell for you to run further commands
 bash
